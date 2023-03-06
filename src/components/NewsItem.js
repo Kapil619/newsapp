75f04039543421b91a933312a6a5c04683c4083e
kapil619 <kapilbadokar321@gmail.com>
2023-03-06 18:26:19 +0530

    Fetching news with Api

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,11 +6,11 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="pic here"/>
+            <img src={!imageUrl?"https://sportshub.cbsistatic.com/i/r/2023/03/05/6113a6d4-8ed7-4e94-959e-a14dbf6a322d/thumbnail/1200x675/a4d07b01f80853031c418f3a7c8ce41f/jon-jones-belt.jpg":imageUrl} className="card-img-top" alt="pic here"/>
             <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...   </p>
-                <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Go somewhere</a>
+                <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read more</a>
             </div>
         </div>
       </div>
