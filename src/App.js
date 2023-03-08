93a08a926ce69beb6629c7876680000215e8ec9f
kapil619 <kapilbadokar321@gmail.com>
2023-03-08 18:35:16 +0530

    Refactoring code(news.js) (DRY)

--- src/App.js
+++ src/App.js
@@ -10,20 +10,21 @@ import {
 } from "react-router-dom";
 
 export default class App extends Component {
+  pageSize = 5;
   render() {
     return (
       <div>
         <Router>
         <NavBar/>
           <Routes> 
-            <Route exact path='/' element={<News key="general" pageSize={5} country="us" category="general"/>}> </Route>
-            <Route exact path='/business' element={<News key="business" pageSize={5} country="us" category="business"/>}></Route>
-            <Route exact path='/entertainment' element={<News key="entertainment" pageSize={5} country="us" category="entertainment" />}></Route>
-            <Route exact path='/general' element={<News key="general" pageSize={5} country="us" category="general" />}> </Route>
-            <Route exact path='/health' element={<News key="health" pageSize={5} country="us" category="health" />}></Route>
-            <Route exact path='/science' element={<News key="science" pageSize={5} country="us" category="science" />}></Route>
-            <Route exact path='/sports' element={<News key="sports" pageSize={5} country="us" category="sports" />} ></Route>
-            <Route exact path='/technology' element={<News key="technology" pageSize={5} country="us" category="technology" />}></Route>
+            <Route exact path='/' element={<News key="general" pageSize= {this.pageSize} country="us" category="general"/>}> </Route>
+            <Route exact path='/business' element={<News key="business" pageSize={this.pageSize} country="us" category="business"/>}></Route>
+            <Route exact path='/entertainment' element={<News key="entertainment" pageSize={this.pageSize} country="us" category="entertainment" />}></Route>
+            <Route exact path='/general' element={<News key="general" pageSize={this.pageSize} country="us" category="general" />}> </Route>
+            <Route exact path='/health' element={<News key="health" pageSize={this.pageSize} country="us" category="health" />}></Route>
+            <Route exact path='/science' element={<News key="science" pageSize={this.pageSize} country="us" category="science" />}></Route>
+            <Route exact path='/sports' element={<News key="sports" pageSize={this.pageSize} country="us" category="sports" />} ></Route>
+            <Route exact path='/technology' element={<News key="technology" pageSize={this.pageSize} country="us" category="technology" />}></Route>
         </Routes>
         </Router>
       </div>
--- src/components/News.js
+++ src/components/News.js
@@ -27,44 +27,56 @@ export class News extends Component {
         }
     }
 
+
+    async updateNews(pageNo){
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c8456e2d173845cab3aaae5dc47d6ab4&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.setState({loading:true});
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        this.setState({articles: parsedData.articles,
+           totalResults: parsedData.totalResults,
+            loading: false}) 
+    }
+
+
     async componentDidMount(){
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c8456e2d173845cab3aaae5dc47d6ab4&pageSize=${this.props.pageSize}`;
-      this.setState({loading:true});
-      let data = await fetch(url);
-      let parsedData = await data.json()
-      this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false})
+     this.updateNews();
     }
 
 
     handlePrevClick = async ()=>{
-        console.log("CLicked");
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c8456e2d173845cab3aaae5dc47d6ab4&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-        this.setState({loading:true});
+        // console.log("CLicked");
+        // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c8456e2d173845cab3aaae5dc47d6ab4&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        // this.setState({loading:true});
         
-        let data = await fetch(url);
-        let parsedData = await data.json()
+        // let data = await fetch(url);
+        // let parsedData = await data.json()
         
-        this.setState({
-            page: this.state.page - 1,
-            articles: parsedData.articles,
-            loading: false
-        })  
+        // this.setState({
+        //     page: this.state.page - 1,
+        //     articles: parsedData.articles,
+        //     loading: false
+        // })  
+        this.setState({ page: this.state.page-1});
+        this.updateNews();
     }
     
     handleNextClick =  async ()=>{
-        console.log("CLicked");
-        if (!(this.state.page +1 >Math.ceil(this.state.totalResults/this.props.pageSize))){
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c8456e2d173845cab3aaae5dc47d6ab4&page=${this.state.page +1}&pageSize=${this.props.pageSize}`;
-        let data = await fetch(url);
-        this.setState({loading:true});
-        let parsedData = await data.json()
+    //     console.log("CLicked");
+    //     if (!(this.state.page +1 >Math.ceil(this.state.totalResults/this.props.pageSize))){
+    //     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c8456e2d173845cab3aaae5dc47d6ab4&page=${this.state.page +1}&pageSize=${this.props.pageSize}`;
+    //     let data = await fetch(url);
+    //     this.setState({loading:true});
+    //     let parsedData = await data.json()
         
-        this.setState({
-            page: this.state.page + 1,
-            articles: parsedData.articles,
-            loading: false
-        })
-    }
+    //     this.setState({
+    //         page: this.state.page + 1,
+    //         articles: parsedData.articles,
+    //         loading: false
+    //     })
+    // }
+    this.setState({ page: this.state.page+1});
+    this.updateNews();
     }
     render() {
         return (
